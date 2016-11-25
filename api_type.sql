542043aa8fa4abac1b095d08df1ac6e8c30557f3
nilszhang <nilszhang@gmail.com>
2016-11-25 23:19:06 +0800

    test

--- api_type.sql
+++
@@ -1,9 +0,0 @@
-INSERT INTO `api_type` VALUES ('1', '1', 'MySQL', '0', 'http://overapi.com/mysql', 1);
-INSERT INTO `api_type` VALUES ('2', '1', 'Java', '0', 'http://overapi.com/java', 1);
-INSERT INTO `api_type` VALUES ('3', '1', 'JavaScript', '0', 'http://overapi.com/javascript', 1);
-INSERT INTO `api_type` VALUES ('4', '1', 'HTML', '0', 'http://overapi.com/html', NULL);
-INSERT INTO `api_type` VALUES ('5', '1', 'jQuery', '0', 'http://overapi.com/jquery', 3);
-INSERT INTO `api_type` VALUES ('6', '1', 'Hibernate', '0', 'http://overapi.com/hibernate', 5);
-INSERT INTO `api_type` VALUES ('7', '1', 'Linux', '1', 'http://overapi.com/linux', 1);
-INSERT INTO `api_type` VALUES ('8', '1', 'CSS', '0', 'http://overapi.com/css', 6);
-INSERT INTO `api_type` VALUES ('9', '1', 'Spring Framework', '1', 'http://overapi.com/spring', NULL);
--- src/main/java/com/jsdx/biz/service/ApiHelpsService.java
+++
@@ -1,9 +0,0 @@
-package com.jsdx.biz.service;
-
-import com.jsdx.biz.entity.ApiHelps;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface ApiHelpsService extends JpaRepository<ApiHelps, String> {
-
-
-}
